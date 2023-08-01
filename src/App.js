ae528f055ddcc45cc88ca358fb2349e2fb171eaa
Aditya8766 <111683825+Aditya8766@users.noreply.github.com>
2023-08-01 13:32:57 +0530

    Practicing react-hook-forms without yup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import { useForm } from 'react-hook-form';
 function App() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [userInfo, setUserInfo] = useState();//only for understanding purpose not related to forms
+  const onSubmit = (data) => {
+    setUserInfo(data)//not relate to forms
+    console.log("data:::", data);
+  }
+  console.log("errors::", errors);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="registration">
+      <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>{/* not related to forms */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form">
+          <div className="fields">
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Enter Your Name" {...register("name", { required: "name is required" })} />
+            <p>{errors.name?.message}</p>
+          </div>
+          <div className="fields">
+            <label>Email</label>
+            <input type="email" name="email" placeholder="Enter Your Email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: { value: /^\S+@\S+$/i, message: "This is not valid email" }
+              })} />
+            <p>{errors.email?.message}</p>
+          </div>
+          <div className="fields">
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Enter Your Password"{...register("password", { required: "Password is required", minLength:{value:4, message:"password must be greater than four characters"}, maxLength:{value:10, message:"password cannot exceed to ten characters"} })} />
+            <p>{errors.password?.message}</p>
+          </div>
+          <button type='submit'>Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
-
 export default App;
